6a9a6a3f64f9e896feacb31d7fc2470ebacbdd89
Eric Dou <lil.reenas@gmail.com>
2020-06-26 22:24:48 +0300

    added event manager

---
+++ Assets/Scripts/CORE/CanvasManager.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanvasManager : MonoBehaviour
+{
+    private void Start()
+    {
+
+    }
+
+    private void Update()
+    {
+
+    }
+
+    public void ButtonLeft()
+    {
+        EventManager.ButtonLeftEvent();
+    }
+
+    public void ButtonRight()
+    {
+        EventManager.ButtonRightEvent();
+    }
+
+    public void ButtonUp()
+    {
+        EventManager.ButtonUpEvent();
+    }
+
+    public void ButtonSwitch()
+    {
+        EventManager.ButtonSwitchEvent();
+    }
+}
---
+++ Assets/Scripts/CORE/EventManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Event Manager")]
+public class EventManager : ScriptableObject
+{
+    public delegate void OnButtonLeftEventDelegate();
+    static public event OnButtonLeftEventDelegate OnButtonLeftEvent;
+    public static void ButtonLeftEvent()
+    {
+        if (OnButtonLeftEvent != null)
+            OnButtonLeftEvent();
+    }
+
+    public delegate void OnButtonRightEventDelegate();
+    static public event OnButtonRightEventDelegate OnButtonRightEvent;
+    public static void ButtonRightEvent()
+    {
+        if (OnButtonRightEvent != null)
+            OnButtonRightEvent();
+    }
+
+    public delegate void OnButtonUpEventDelegate();
+    static public event OnButtonUpEventDelegate OnButtonUpEvent;
+    public static void ButtonUpEvent()
+    {
+        if (OnButtonLeftEvent != null)
+            OnButtonLeftEvent();
+    }
+
+    public delegate void OnButtonSwitchEventDelegate();
+    static public event OnButtonSwitchEventDelegate OnButtonSwitchEvent;
+    public static void ButtonSwitchEvent()
+    {
+        if (OnButtonSwitchEvent != null)
+            OnButtonSwitchEvent();
+    }
+}
