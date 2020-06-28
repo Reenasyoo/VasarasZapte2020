dc9924cec66a01dd8fffb34e51f643ecd3996ee4
jettucis <aleharijs@gmail.com>
2020-06-28 14:32:14 +0300

    additional errors

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D)]
+[RequireComponent(typeof(BoxCollider2D))]
 public class Water : MonoBehaviour
 {
     public BoxCollider2D kolis;
