742cb38e30748198fb82cfc2d641d489b7c1134a
jettucis <aleharijs@gmail.com>
2020-06-28 13:42:13 +0300

    works on water mehatronix

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -2,17 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(BoxCollider2D))]
 public class Water : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public BoxCollider2D kolis;
+
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        switch (this.gameObject.tag)
+        {
+            case "Blue":
+                {
+                    if (collision.tag == "Male") 
+                    {
+                    
+                    }
+                    break;
+                }
+            case "Red":
+                {
+                    if (collision.tag == "Female")
+                    {
+                        
+                    }
+                    break;
+                }
+            default:
+                {
+                    break;
+                }
+
+        }
     }
 }
